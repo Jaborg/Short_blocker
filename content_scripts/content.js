5ead57e1a1aa2f6b1b5e9012a17c8d80d07a66a3
Jaborg <jlappinsadiq@gmail.com>
2023-10-22 21:21:34 +0100

    Blocking Shorts html

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,2 +1,22 @@
-console.log("Content script running");
-console.log(document.documentElement.outerHTML);
+console.log("Content script started");
+
+// Find the specific span element with id="title" and content "Shorts"
+
+
+// Find the specific span element with id="title" and content "Shorts"
+let targetSpan = Array.from(document.querySelectorAll('span#title.style-scope.ytd-rich-shelf-renderer')).find(span => span.textContent.trim() === 'Shorts');
+
+// If the span is found, traverse upwards to find the closest parent <div> with id="content"
+if (targetSpan) {
+    let parentDivWithIdContent = targetSpan.closest('div#content');
+    console.log(parentDivWithIdContent);
+
+
+    // If the parent <div> with id="content" is found, delete it
+    if (parentDivWithIdContent) {
+        parentDivWithIdContent.remove();
+    }
+}
+
+
+console.log("Content script finished");
